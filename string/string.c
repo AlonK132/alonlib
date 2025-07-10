705572064d5dc7b0718a5303506a6f990a37596a
AlonKamenetsky <kamnitzk@post.bgu.ac.il>
2025-07-10 23:33:15 +0300

    memchr implementation

--- string/string.c
+++ string/string.c
@@ -3,3 +3,15 @@
 //
 
 #include "string.h"
+void *al_memchr(const void *str, int c, size_t n){
+    if(!str)
+        return NULL;
+    const unsigned char *p = (const unsigned char *)str;
+    unsigned char target = (unsigned char)c;
+    for(size_t i = 0; i < n; i++){
+        if(p[i] == target){
+            return (void*)&p[i];
+        }
+    }
+    return NULL;
+}
--- string/string.h
+++ string/string.h
@@ -4,5 +4,6 @@
 
 #ifndef ALONLIB_STRING_H
 #define ALONLIB_STRING_H
-
+#include <stddef.h>
+void *al_memchr(const void *str, int c, size_t n);
 #endif //ALONLIB_STRING_H
