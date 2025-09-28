814488b2ada191a01d8d7526e4f404f72bc153a5
AlonKamenetsky <kamnitzk@post.bgu.ac.il>
2025-09-28 12:38:42 +0300

    added comments

--- string/string.h
+++ string/string.h
@@ -5,7 +5,11 @@
 #ifndef ALONLIB_STRING_H
 #define ALONLIB_STRING_H
 #include <stddef.h>
+
+// Searches the first n bytes of str for the first occurrence of byte c
 void *al_memchr(const void *str, int c, size_t n);
+
+// Returns the length of the null-terminated string s
 size_t al_strlen(const char *s);
 
 #endif //ALONLIB_STRING_H
